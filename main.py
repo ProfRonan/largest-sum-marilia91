935a8c1aeb3c3dfde6ba1c89462759f5e0e72ba0
marilia91 <marilia.iza112@gmail.com>
2023-05-28 16:14:26 -0300

    largest sum adicionado

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
+    if len(numbers) < 2:
+        return None
+    numbers.sort(reverse=True)
+    primeiro = numbers[1]  # o primeiro número da soma
+    segundo = numbers[0]  # o segundo número da soma
     return primeiro, segundo
